f5d2b8b63cc4201d037a96b75091dc52cd17e37b
priyarai05 <pri2605@gmail.com>
2024-02-23 12:21:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let questionContainer = document.querySelectorAll('.question');
+
+
+questionContainer.forEach(item => {
+    let answer = item.getElementsByClassName("answer")
+    let img = item.querySelectorAll('img')
+
+    img[0].addEventListener("click", () => {
+        if (answer[0].classList.contains("tgl")) {
+            answer[0].classList.remove("tgl")
+            img[0].src = "icon-minus.svg"
+        } else {
+            answer[0].classList.add("tgl")
+            img[0].src = "icon-plus.svg"
+        }
+    })
+});
